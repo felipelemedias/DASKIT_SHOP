2590ffdf441bc2e281eefc4e79ccf45ff825eb1b
fleme <fleme@DESKTOP-PLBVS5T>
2024-03-06 11:10:37 -0300

    add singleton e factory no projeto

---
+++ src/main/java/com/mycompany/main/Apple.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author fleme
+ */
+public class Apple implements FabricanteCelular {
+    
+    @Override
+    public Celular constroiCelular(String modelo){
+        if(modelo.equals("iphones")){
+            return new IphoneS();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/mycompany/main/Galaxy8.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author fleme
+ */
+public class Galaxy8 implements Celular {
+
+    @Override
+    public void fazLigacao() {
+        System.out.println("Galaxy8 fazendo ligação..");
+    }
+
+    @Override
+    public void tiraFoto() {
+        System.out.println("Galaxy8 tirando foto..");
+    }
+    
+}
